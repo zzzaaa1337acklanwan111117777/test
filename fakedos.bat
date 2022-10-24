9ac8d43af5ac35229806c742fe021ca75b213a91
zzzaaa1337acklanwan111117777 <113130604+zzzaaa1337acklanwan111117777@users.noreply.github.com>
2022-10-24 08:29:22 -0400

    Add files via upload

---
+++ fakedos.bat
@@ -0,0 +1,10 @@
+Echo Off
+Echo:
+Echo  ÚÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄ¿
+Echo  ³                                                         ³
+Echo  ³ Arbitrary download and execution!!!                     ³
+Echo  ³                                                         ³
+Echo  ³                                                         ³
+Echo  ÀÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÙ
+Echo:
+Pause
